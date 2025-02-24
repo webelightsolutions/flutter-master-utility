c9e1787bab4093a0192971e44710acdf73b5b2b2
mahammada_webelight <mahammada@webelight.co.in>
2025-02-24 14:39:22 +0530

    Jailbreak issue for android 35 SDK

--- lib/src/root_device_helper.dart
+++ lib/src/root_device_helper.dart
@@ -1,24 +1,24 @@
-// Package imports:
+// // Package imports:
 
-// Package imports:
-import 'package:jailbreak_root_detection/jailbreak_root_detection.dart';
+// // Package imports:
+// import 'package:jailbreak_root_detection/jailbreak_root_detection.dart';
 
-// Project imports:
-import '../master_utility.dart';
+// // Project imports:
+// import '../master_utility.dart';
 
-class RootDeviceHelper {
-  /// root device checker, it will return true or false.
-  static Future<bool> checkJailBreak() async {
-    bool jailBroken = false;
+// class RootDeviceHelper {
+//   /// root device checker, it will return true or false.
+//   static Future<bool> checkJailBreak() async {
+//     bool jailBroken = false;
 
-    try {
-      jailBroken = await JailbreakRootDetection.instance.isJailBroken;
-    } catch (e) {
-      jailBroken = true;
-    }
+//     try {
+//       jailBroken = await JailbreakRootDetection.instance.isJailBroken;
+//     } catch (e) {
+//       jailBroken = true;
+//     }
 
-    LogHelper.logSuccess("Is device Jailbroken or Rooted : $jailBroken");
+//     LogHelper.logSuccess("Is device Jailbroken or Rooted : $jailBroken");
 
-    return jailBroken;
-  }
-}
+//     return jailBroken;
+//   }
+// }
