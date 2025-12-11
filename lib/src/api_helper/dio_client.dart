9d496f210d35ed77f2e061e3e3a94c8fff419dbf
kurbanalim_webelight <kurbanalim@webelight.co.in>
2025-12-11 16:09:32 +0530

    Downgrade device_info_plus dependency to version 11.3.0. Refactor DioClient for improved readability by adjusting formatting in the logging interceptor and token refresh logic. Update CustomErrorMapper typedef for better clarity.

--- lib/src/api_helper/dio_client.dart
+++ lib/src/api_helper/dio_client.dart
@@ -35,7 +35,8 @@ class DioClient {
     }
 
     if (_isApiLogVisible) {
-      interceptors.add(HttpFormatter(loggingFilter: (request, response, error) => true));
+      interceptors.add(
+          HttpFormatter(loggingFilter: (request, response, error) => true));
       interceptors.add(CurlLoggerDioInterceptor(printOnSuccess: true));
     }
 
@@ -70,9 +71,10 @@ class DioClient {
         tokenStorage: config.tokenStorage,
         baseClient: _dio ?? Dio(),
         onRefresh: (refreshClient, refreshToken) async {
-          refreshClient.options =
-              refreshClient.options.copyWith(headers: {config.refreshTokenHeaderKey: 'Bearer $refreshToken'});
-          final response = await refreshClient.post(config.refreshTokenEndPoint);
+          refreshClient.options = refreshClient.options.copyWith(
+              headers: {config.refreshTokenHeaderKey: 'Bearer $refreshToken'});
+          final response =
+              await refreshClient.post(config.refreshTokenEndPoint);
           final token = config.responseMapper(response.data);
           return token;
         },
